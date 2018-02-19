ed2df37005cd6a19957bfc64aec64becfc513dd9
Mataan Abucar <mabucar88@gmail.com>
2018-02-19 14:35:57 -0500

    done

---
+++ tdd-exercises-pair/Exercises.Tests/CalcTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDExercises.Tests
+{
+    [TestClass]
+    public class CalcTest
+    {
+
+
+        [TestMethod]
+        public void SumTestNull()
+        {
+            StringCalculator calc = new StringCalculator();
+            Assert.AreEqual(0, calc.Add(""));
+        }
+
+
+
+        [TestMethod]
+        public void SumTest()
+        {
+            StringCalculator calc = new StringCalculator();
+            Assert.AreEqual(33, calc.Add("33"));
+
+
+        }
+
+
+        [TestMethod]
+        public void SumTest1Plus2()
+        {
+            StringCalculator calc = new StringCalculator();
+            Assert.AreEqual(3, calc.Add("1,2"));
+
+
+        }
+
+        [TestMethod]
+        public void SumTestMultiNum()
+        {
+            StringCalculator calc = new StringCalculator();
+            Assert.AreEqual(13, calc.Add("1,2,4,6"));
+
+
+        }
+
+
+        [TestMethod]
+        public void SumTestNewLine()
+        {
+            StringCalculator calc = new StringCalculator();
+            Assert.AreEqual(6, calc.Add("1\n2,3"));
+
+
+        }
+
+
+        [TestMethod]
+        public void SumTestCrazySymbols()
+        {
+            StringCalculator calc = new StringCalculator();
+            Assert.AreEqual(3, calc.Add("//;\n1;2"));
+
+
+        }
+
+    }
+}
---
+++ tdd-exercises-pair/Exercises/StringCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExercises
+{
+    public class StringCalculator
+    {
+
+        /// <summary>
+        /// intput string of anything with number, add number's in string together!
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns>returns sum of numbers in string</returns>
+        /// 
+        public int Add(string numbers)
+        {
+            int result = 0;
+            
+            
+            char delimeter = ';';
+            
+            if (numbers.Contains('\n')||(numbers.Contains(',')))
+            {
+                string[] multiNum;
+
+                multiNum = numbers.Split('\n',',',delimeter);
+
+                foreach (string n in multiNum)
+                {
+                    int.TryParse(n, out int num);
+
+                    result += num;
+                }
+            }
+            else
+            {
+                int.TryParse(numbers, out int singleNum);
+
+                result = singleNum;
+            }
+
+            return result;
+        }
+
+    }
+}
